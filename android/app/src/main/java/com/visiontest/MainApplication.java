272603f6f2240c01bbd246a9f59e365180731cb9
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-04-20 15:30:20 -0700

    Show photos from Camera Roll

--- android/app/src/main/java/com/visiontest/MainApplication.java
+++ android/app/src/main/java/com/visiontest/MainApplication.java
@@ -3,6 +3,7 @@ package com.visiontest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
             new RNFetchBlobPackage()
       );
     }
--- index.ios.js
+++ index.ios.js
@@ -7,27 +7,127 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Button,
+  CameraRoll,
+  Dimensions,
+  Image,
+  Modal,
+  ScrollView,
   StyleSheet,
   Text,
-  View
+  TouchableHighlight,
+  View,
 } from 'react-native';
 
+import Share from 'react-native-share'
+import RNFetchBlob from 'react-native-fetch-blob'
+
+const {width}  = Dimensions.get('window')
+
 export default class VisionTest extends Component {
+  state = {
+    modalVisible: false,
+    photos: [],
+    index: null
+  }
+
+  setIndex = (index) => {
+    if (index === this.state.index) {
+      index = null
+    }
+    this.setState({ index })
+  }
+
+  getPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: 'All'
+    })
+    .then(r => this.setState({ photos: r.edges }))
+  }
+
+  toggleModal = () => {
+    this.setState({ modalVisible: !this.state.modalVisible });
+  }
+
+  navigate = () => {
+    const { navigate } = this.props.navigation
+    navigate('ImageBrowser')
+  }
+
+  share = () => {
+    const image = this.state.photos[this.state.index].node.image.uri
+    RNFetchBlob.fs.readFile(image, 'base64')
+    .then((data) => {
+      let shareOptions = {
+        title: "React Native Share Example",
+        message: "Check out this photo!",
+        url: `data:image/jpg;base64,${data}`,
+        subject: "Check out this photo!"
+      }
+
+      Share.open(shareOptions)
+        .then((res) => console.log('res:', res))
+        .catch(err => console.log('err', err))
+    })
+  }
+
   render() {
+    console.log('state :', this.state)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Button
+          title='View Photos'
+          onPress={() => { this.toggleModal(); this.getPhotos() }}
+        />
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => console.log('closed')}
+        >
+          <View style={styles.modalContainer}>
+            <Button
+              title='Close'
+              onPress={this.toggleModal}
+            />
+            <ScrollView
+              contentContainerStyle={styles.scrollView}>
+              {
+                this.state.photos.map((p, i) => {
+                  return (
+                    <TouchableHighlight
+                      style={{opacity: i === this.state.index ? 0.5 : 1}}
+                      key={i}
+                      underlayColor='transparent'
+                      onPress={() => this.setIndex(i)}
+                    >
+                      <Image
+                        style={{
+                          width: width/3,
+                          height: width/3
+                        }}
+                        source={{uri: p.node.image.uri}}
+                      />
+                    </TouchableHighlight>
+                  )
+                })
+              }
+            </ScrollView>
+            {
+              this.state.index !== null  && (
+                <View style={styles.shareButton}>
+                  <Button
+                      title='Share'
+                      onPress={this.share}
+                    />
+                </View>
+              )
+            }
+          </View>
+        </Modal>
       </View>
-    );
+    )
   }
 }
 
@@ -35,18 +135,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  modalContainer: {
+    paddingTop: 20,
+    flex: 1
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  scrollView: {
+    flexWrap: 'wrap',
+    flexDirection: 'row'
+  },
+  shareButton: {
+    position: 'absolute',
+    width,
+    padding: 10,
+    bottom: 0,
+    left: 0
   },
 });
 
