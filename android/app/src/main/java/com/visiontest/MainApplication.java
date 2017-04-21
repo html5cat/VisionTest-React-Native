0fbf55e78109a3e2f71d14f284a4c72293e7a3a1
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-04-20 17:09:34 -0700

    Updload Photos to S3

--- android/app/src/main/java/com/visiontest/MainApplication.java
+++ android/app/src/main/java/com/visiontest/MainApplication.java
@@ -3,6 +3,8 @@ package com.visiontest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import cl.json.RNSharePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RCTCameraPackage(),
             new RNSharePackage(),
             new RNFetchBlobPackage()
       );
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, CameraRoll, Dimensions, View, StyleSheet } from 'react-native';
 import Camera from 'react-native-camera';
-import { Button } from 'react-native-vector-icons/Ionicons';
+// import { Button } from 'react-native-vector-icons/Ionicons';
 import { RNS3 } from 'react-native-aws3';
 
+import { awsConfig } from './aws.config';
 
-class VisionTest extends Component {
+
+class CameraApp extends Component {
   constructor() {
     super();
     this.takePicture = this.takePicture.bind(this);
@@ -20,18 +22,9 @@ class VisionTest extends Component {
           type: 'image/jpeg'
         };
 
-        const options = {
-          keyPrefix: 'photos/',
-          bucket: 'html5cat-vision-test',
-          region: 'us-west-2',
-          accessKey: 'AKIAJ2J7QAZY3JUJYTZA',
-          secretKey: '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',
-          successActionStatus: 201
-        }
+        const options = awsConfig
 
-        RNS3.put(file, options)
-        .progress((e) => console.log(e.loaded / e.total))
-        .then(response => {
+        RNS3.put(file, options).then(response => {
           if (response.status !== 201) {
             throw new Error('Failed to upload image to S3', response);
           }
@@ -83,4 +76,4 @@ const styles = StyleSheet.create({
   }
 });
 
-AppRegistry.registerComponent('VisionTest', () => VisionTest);
+export default App
